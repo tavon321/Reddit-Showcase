fe89fa7ae9dd78b732b65006fb441efdf3e4097f
Gustavo Londoño <tavon321@gmail.com>
2021-06-09 07:28:58 -0500

    `loadFeed` message client with url

--- Reddit-Showcase/Reddit-ShowcaseTests/Remote/RemoteRedditTopFeedLoaderTests.swift
+++ Reddit-Showcase/Reddit-ShowcaseTests/Remote/RemoteRedditTopFeedLoaderTests.swift
@@ -8,27 +8,51 @@
 import XCTest
 
 protocol HTTPClient {
+    func load(url: URL)
 }
 
 class RemoteRedditTopFeedLoader {
+    private let url: URL
     private let client: HTTPClient
     
-    init(client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
+        self.url = url
         self.client =  client
     }
+    
+    func load() {
+        client.load(url: url)
+    }
 }
-
 
 class RemoteRedditTopFeedLoaderTests: XCTestCase {
     func test_init_doesNotMessageClient() {
         let client = HTTPClientSpy()
-        _ = RemoteRedditTopFeedLoader(client: client)
+        _ = RemoteRedditTopFeedLoader(url: anyURL, client: client)
         
         XCTAssertTrue(client.callCount == 0)
     }
     
+    func test_loadFeed_messageClientWithURL() {
+        let expectedUrl = anyURL
+        let client = HTTPClientSpy()
+        let sut = RemoteRedditTopFeedLoader(url: expectedUrl, client: client)
+        
+        sut.load()
+        
+        XCTAssertEqual(client.requestedUrls, [expectedUrl])
+    }
+    
     // MARK: - Helpers
-    class HTTPClientSpy: HTTPClient {
+    private let anyURL = URL(string: "https://any-url.com")!
+    
+    private class HTTPClientSpy: HTTPClient {
+        private(set) var requestedUrls = [URL]()
         var callCount = 0
+        
+        func load(url: URL) {
+            requestedUrls.append(url)
+            callCount += 1
+        }
     }
 }
