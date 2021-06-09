91502b171c639f31d47780ffb1f9f6af9b7771bb
Gustavo Londoño <tavon321@gmail.com>
2021-06-08 19:50:22 -0500

    add RedditFeed Model with the test requirements

---
+++ Reddit-Showcase/Reddit-Showcase/Feature/RedditFeed.swift
@@ -0,0 +1,18 @@
+//
+//  RedditFeed.swift
+//  Reddit-Showcase
+//
+//  Created by Gustavo on 8/06/21.
+//
+
+import Foundation
+
+public struct RedditFeed {
+    let title: String
+    let author: String
+    let entryDate: Date
+    let numberOfComments: String
+    let thumbnail: URL?
+    let imageURL: URL?
+    let visited: Bool
+}
