2997ad3d3c654359f9cfc9f3312160bc84582925
Gustavo Londoño <tavon321@gmail.com>
2021-06-10 19:11:57 -0500

    `DidStartLoading` display loading viewmodel
    Ok so i this is the view model that i am going to present on each cells the idea is load the image while is loading is in true and set the thumbnaiil to nil

--- Reddit-Showcase/Reddit-ShowcaseTests/Presentation/ImagePresenterTests.swift
+++ Reddit-Showcase/Reddit-ShowcaseTests/Presentation/ImagePresenterTests.swift
@@ -6,9 +6,20 @@
 //
 
 import XCTest
+import Reddit_Showcase
+
+public struct FeedImageViewModel {
+    public let title: String
+    public let author: String
+    public let elapsedInterval: String
+    public let numberOfComments: String
+    public let imageURL: URL?
+    public let thumnail: UIImage?
+    public let isLoading: Bool
+}
 
 public protocol ImagePresenterView {
-    
+    func display(_ model: FeedImageViewModel)
 }
 
 public class ImagePresenter {
@@ -17,16 +28,44 @@ public class ImagePresenter {
     public init(view: ImagePresenterView) {
         self.view = view
     }
+    
+    func didStartLoadingImageData(for model: FeedViewModel) {
+        view.display(.init(title: model.title,
+                           author: model.author,
+                           elapsedInterval: model.elapsedInterval,
+                           numberOfComments: model.numberOfComments,
+                           imageURL: model.imageURL,
+                           thumnail: nil,
+                           isLoading: true))
+    }
 }
 
 class ImagePresenterTests: XCTestCase {
     func test_init_doesNotMessageView() {
         let (_, view) = makeSUT()
         
-        XCTAssertEqual(view.messages, 0)
+        XCTAssertEqual(view.messages.count, 0)
+    }
+    
+    func test_didStartLoadingImageData_displaysLoadingImage() {
+        let (sut, view) = makeSUT()
+        let model = feedModel
+        
+        sut.didStartLoadingImageData(for: model)
+        
+        let message = view.messages.first
+        XCTAssertEqual(view.messages.count, 1)
+        XCTAssertEqual(message?.title, model.title)
+        XCTAssertEqual(message?.author, model.author)
+        XCTAssertEqual(message?.elapsedInterval, model.elapsedInterval)
+        XCTAssertEqual(message?.numberOfComments, model.numberOfComments)
+        XCTAssertEqual(message?.imageURL, model.imageURL)
+        XCTAssertEqual(message?.isLoading, true)
     }
     
     // MARK: Helpers
+    private var feedModel = FeedViewModel(item: sampleFeed)
+    
     private func makeSUT(file: StaticString = #file,
                          line: UInt = #line) -> (sut: ImagePresenter, view: ImagePresenterViewSpy) {
         let view = ImagePresenterViewSpy()
@@ -38,6 +77,10 @@ class ImagePresenterTests: XCTestCase {
     }
     
     private class ImagePresenterViewSpy: ImagePresenterView {
-        private(set) var messages = 0
+        private(set) var messages = [FeedImageViewModel]()
+        
+        func display(_ model: FeedImageViewModel) {
+            messages.append(model)
+        }
     }
 }
