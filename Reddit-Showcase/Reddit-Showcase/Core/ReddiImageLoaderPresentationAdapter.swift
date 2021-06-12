e5beb7c8b521b4c8cc9a30343cddeb83dc98f55a
Gustavo Londoño <tavon321@gmail.com>
2021-06-12 11:03:37 -0500

    load image from url and save it to lib
    I am no going to hablde errors here because the test didn't ask me too

--- Reddit-Showcase/Reddit-Showcase/Core/ReddiImageLoaderPresentationAdapter.swift
+++ Reddit-Showcase/Reddit-Showcase/Core/ReddiImageLoaderPresentationAdapter.swift
@@ -8,19 +8,18 @@
 import UIKit
 
 class ReddiImageLoaderPresentationAdapter<View: ImagePresenterView, Image>: CellControllerDelegate where View.Image == Image {
-   
-    
-    
     private let model: FeedViewModel
     private let imageLoader: ImageDataLoader
+    private let imageSaver: ImageSaver
     private var cache = NSCache<NSURL, NSData>()
     private var cancellable: HTTPClientTask?
     
     var presenter: ImagePresenter<View, Image>?
     
-    init(model: FeedViewModel, imageLoader: ImageDataLoader) {
+    init(model: FeedViewModel, imageLoader: ImageDataLoader, imageSaver: ImageSaver) {
         self.model = model
         self.imageLoader = imageLoader
+        self.imageSaver = imageSaver
     }
     
     func didRequestImage(with url: URL) {
@@ -44,6 +43,18 @@ class ReddiImageLoaderPresentationAdapter<View: ImagePresenterView, Image>: Cell
     }
     
     func didRequestSaveImage(with url: URL) {
+        _ = imageLoader.loadImageData(from: url) { [weak self] result in
+            switch result {
+            case .success(let data):
+                self?.imageSaver.save(data, completion: { error in
+                    if error {
+                        print("save image error \(error)")
+                    }
+                })
+            case .failure(let error):
+                print("load image error \(error)")
+            }
+        }
     }
     
     func didCancelImageRequest() {
